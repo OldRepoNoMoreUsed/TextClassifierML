c669adace2dd7183be7b1a2f1cef460411d76a0a
SommerNicolas <nicolas.sommer@he-arc.ch>
2017-04-05 10:39:57 +0200

    Create dir tree and select random file

--- pretraitement.py
+++ pretraitement.py
@@ -1,1 +1,41 @@
+"""Module de prétraitement des données.
+Utile pour la conversation des noms, adjectif, verbe et adverbe
+selon la notation canonique du texte. Puis séparation dans les
+dossiers de training ou de test (pos/neg) en respectant un quota
+de 80 - 20 (training - test)"""
+
+import os
+import random
+import shutil
+
+def select_random_training_file(dir_path):
+    """Choisi les fichiers qui feront parti du corpus d'entrainement"""
+    training_files = []
+    while len(training_files) < 400:
+        choose_file = random.choice(os.listdir(dir_path))
+        if choose_file not in training_files:
+            training_files.append(choose_file)
+    return training_files
+
+def create_dir():
+    """Crée l'arborescence de dossier utile"""
+    shutil.rmtree("TestText")
+    shutil.rmtree("TrainingText")
+    os.makedirs("TrainingText/neg")
+    os.makedirs("TrainingText/pos")
+    os.makedirs("TestText/neg")
+    os.makedirs("TestText/pos")
+    print("TrainingText directories create")
+    print("TestText directories create")
+
+
+if __name__ == "__main__":
+    create_dir()
+    print("Select file for training")
+    negative_files = select_random_training_file("tagged/neg/")
+    print("Selected negative files: " + str(negative_files))
+    positive_files = select_random_training_file("tagged/pos/")
+    print("\nSelected positive files: " + str(positive_files))
+
+
 
